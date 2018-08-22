2e1b19f5d910815012d1d0a0a05b8f3308603d5d
training <marcin.danilewicz@mobica.com>
2018-08-22 15:34:00 +0200

    Project Lesson 2 done

---
+++ SmackChat/SmackChat/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  SmackChat
+//
+//  Created by training on 22.08.2018.
+//  Copyright © 2018 Developers. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+}
---
+++ SmackChat/SmackChat/Controller/ChatVC.swift
@@ -0,0 +1,26 @@
+//
+//  ChatVC.swift
+//  SmackChat
+//
+//  Created by training on 22.08.2018.
+//  Copyright © 2018 Developers. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //Outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+}
